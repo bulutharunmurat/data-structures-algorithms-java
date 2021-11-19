0a9f8208a21877ffa605dd331c5ceee921c576f7
bulutharunmurat <bulutharunmurat@gmail.com>
2021-11-19 22:58:00 +0300

    Add queue

---
+++ src/queue/Queue.java
@@ -0,0 +1,63 @@
+package queue;
+
+public class Queue {
+
+    private Integer front = 0;
+    private Integer rear = -1;
+    private Integer capacity;
+    private Integer[] array;
+    private Integer count = 0;
+
+    public Queue(Integer capacity) {
+        this.capacity = capacity;
+        this.array = new Integer[capacity];
+    }
+
+    public boolean isEmpty(){
+        return count == 0;
+    }
+
+    public boolean isFull(){
+        return count == capacity;
+    }
+
+    public void enqueue(Integer variable){
+        if (!isFull()){
+            count = count + 1;
+            rear = (rear + 1) % capacity;
+            array[rear] = variable;
+        }
+    }
+
+    public Integer dequeue() throws Exception {
+        if (!isEmpty()){
+            count = count - 1;
+            Integer variable = array[front];
+            front = (front + 1) % capacity;
+            array[rear] = variable;
+            return variable;
+        }
+        throw new Exception("Stack is Full");
+    }
+
+    public void clear(){
+        rear = -1;
+        front = 0;
+    }
+
+    @Override
+    public String toString() {
+
+        String queue = "";
+
+        for (int i = 0; i <= count; i++){
+            queue = queue + array[(front + i) % capacity ] + ", ";
+        }
+        return "Queue{" +
+                "top=" + rear +
+                ", capacity=" + capacity +
+                ", array=" + queue +
+                '}';
+    }
+
+}
